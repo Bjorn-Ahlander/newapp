334b230057d81c2343946362e8b3ed96e91cc92d
Bjorn-Ahlander <ahlander.bjorn@telia.com>
2023-12-07 13:35:38 +0100

    Changes -Ömer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Comp1 from './components/Comp1';
+//import data from './components/Data.js';
+import data from './components/Data.json';
+
+// const data =[{
+//   firstName: "John",
+//   lastName: "Doe",
+//   id: 1
+// },
+// {
+//   firstName: "Johan",
+//   lastName: "Doe",
+//   id: 2
+// },
+// {
+//   firstName: "Sara",
+//   lastName: "Doe",
+//   id: 3
+// },
+// {
+//   firstName: "Henrik",
+//   lastName: "Johansson",
+//   id: 4
+// }
+// ];
+
+const allData = (values) => {
+  return(
+    <>
+      <Comp1
+        firstName={values.firstName}
+        lastName={values.lastName}
+        id= {values.id}
+        src={values.src}
+      />
+    </>
+  )
+}
+
+//console.log(data[3].lastName);
 
 function App() {
+  
   return (
     <div className="App">
-      <h3>bjorntest2</h3>
+      <h1>Props</h1>
+      {/* <Comp1
+        firstName={data.firstName}
+        lastName={data.lastName}
+        id={data.id}
+       
+      /> */}
+      {/* <Comp1
+        {...data}
+      /> */}
+
+      {data.map(allData)} 
     </div>
   );
 }
 
 export default App;
+
